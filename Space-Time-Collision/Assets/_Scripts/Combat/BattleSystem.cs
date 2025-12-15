b83e3d5b296ebfa69a59b1d44b0026d106985dda
DnDean3326 <94852868+DnDean3326@users.noreply.github.com>
2025-12-14 19:58:15 -0600

    Combat Initiative Update

--- Space-Time-Collision/Assets/_Scripts/Combat/BattleSystem.cs
+++ Space-Time-Collision/Assets/_Scripts/Combat/BattleSystem.cs
@@ -10,9 +10,11 @@ public class BattleSystem : MonoBehaviour
     private enum BattleState
     {
         Start,
+        Battle,
+        Ordering,
         PlayerTurn,
         EnemyTurn,
-        Battle,
+        Ability,
         Won,
         Lost,
     }
@@ -28,16 +30,25 @@ public class BattleSystem : MonoBehaviour
     [SerializeField] private List<BattleEntities> allCombatants = new List<BattleEntities>();
     [SerializeField] private List<BattleEntities> enemyCombatants = new List<BattleEntities>();
     [SerializeField] private List<BattleEntities> partyCombatants = new List<BattleEntities>();
+    [SerializeField] private List<BattleEntities> preparedCombatants = new List<BattleEntities>();
     
-    /*[Header("UI")]
-        Work in progress
-    */
+    [Header("UI")]
+    [SerializeField] private GameObject combatStartUI;
+    [SerializeField] private Animator combatStartUIAnimator;
     
     private PartyManager partyManager;
+    
     private EnemyManager enemyManager;
     private int currentPlayer;
-
+    
+    private const float COMBAT_BEGIN_DELAY = 1f;
     private const float TURN_ACTION_DELAY = 2f;
+    private const int TURN_START_THRESHOLD = 200;
+    private const int BASE_INITIATIVE_GAIN = 20;
+    private const int MAX_INITIATIVE_START = 100;
+    
+    // Animator Constants
+    private const string BATTLE_START_END = "EndTrigger";
 
     private void Awake()
     {
@@ -50,9 +61,84 @@ public class BattleSystem : MonoBehaviour
     {
         CreatePartyEntities();
         CreateEnemyEntities();
+        state = BattleState.Start;
+        StartCoroutine(StartRoutine());
     }
 
     // Battle state IENumerators go here
+
+    private IEnumerator StartRoutine()
+    {
+        if (state == BattleState.Start) {
+            for (int i = 0; i < allCombatants.Count; i++) {
+                allCombatants[i].initiative = Random.Range(90, MAX_INITIATIVE_START + 1);
+                print(allCombatants[i].name + "'s starting initiative is " +  allCombatants[i].initiative);
+            }
+
+            yield return new WaitForSeconds(COMBAT_BEGIN_DELAY);
+            combatStartUIAnimator.SetTrigger(BATTLE_START_END);
+            yield return new WaitForSeconds(0.2f);
+            state = BattleState.Battle;
+            StartCoroutine(BattleRoutine());
+            yield return null;
+        } else {
+            print("Start Routine called but the battle system is not in the start state.");
+        }
+        yield return null;
+    }
+
+    private IEnumerator BattleRoutine()
+    {
+        print("Battle Routine called");
+        do {
+            for (int i = 0; i < allCombatants.Count; i++) {
+                if (state == BattleState.Battle) {
+                    allCombatants[i].initiative += BASE_INITIATIVE_GAIN + allCombatants[i].speed;
+                    print(allCombatants[i].name + "'s new initiative is " +  allCombatants[i].initiative);
+                    if (allCombatants[i].initiative >= TURN_START_THRESHOLD) {
+                        preparedCombatants.Add(allCombatants[i]);
+                        //allCombatants[i].initiative -= TURN_START_THRESHOLD;
+                        print(allCombatants[i].name + " has been prepared.");
+                    }
+                }
+            }
+            yield return new WaitForSeconds(0.5f);
+        } while (preparedCombatants.Count <= 0);
+
+        state = BattleState.Ordering;
+        StartCoroutine(OrderRoutine());
+        yield return null;
+    }
+
+    private IEnumerator OrderRoutine()
+    {
+        if (state == BattleState.Ordering) {
+            // Sorts prepared combatants by initiative from highest to lowest
+            preparedCombatants.Sort((bi1, bi2) => -bi1.initiative.CompareTo(bi2.initiative));
+            int characterIndex = allCombatants.IndexOf(preparedCombatants[0]);
+            if (preparedCombatants[0].isPlayer) {
+                state = BattleState.PlayerTurn;
+                StartCoroutine(PartyTurnRoutine(characterIndex));
+            } else if (!preparedCombatants[0].isPlayer) {
+                state = BattleState.EnemyTurn;
+                StartCoroutine(EnemyTurnRoutine(characterIndex));
+            }
+                
+        }
+        yield return null;
+    }
+
+    private IEnumerator PartyTurnRoutine(int characterIndex)
+    {
+        print("Player turn has begun. " + allCombatants[characterIndex].name + " Is the active character.");
+        yield return null;
+    }
+
+    private IEnumerator EnemyTurnRoutine(int characterIndex)
+    {
+        print("Enemy turn has begun. " + allCombatants[characterIndex].name + " Is the active enemy.");
+        yield return null;
+    }
 
     private void CreatePartyEntities()
     {
@@ -94,7 +180,7 @@ public class BattleSystem : MonoBehaviour
             
             tempEntity.SetEntityValue(currentEnemies[i].enemyName, currentEnemies[i].level, currentEnemies[i].maxHealth, currentEnemies[i].currentHealth,
                 currentEnemies[i].maxSpirit, currentEnemies[i].currentSpirit, currentEnemies[i].maxDefense, currentEnemies[i].maxArmor, currentEnemies[i].power,
-                currentEnemies[i].skill, currentEnemies[i].wit, currentEnemies[i].mind, currentEnemies[i].speed, currentEnemies[i].luck, true);
+                currentEnemies[i].skill, currentEnemies[i].wit, currentEnemies[i].mind, currentEnemies[i].speed, currentEnemies[i].luck, false);
             
             // Spawn the visuals
             // Right now it sets to a set position based on instatiate order, this will eventually need to be updated to place on the selected grid position
@@ -153,7 +239,9 @@ public class BattleEntities
     public Action battleAction;
 
     public string name;
+    public bool isPlayer;
     public int level;
+    public int initiative;
 
     public int maxHealth;
     public int currentHealth;
@@ -180,6 +268,7 @@ public class BattleEntities
         int entitySkill, int entityWit, int entityMind, int entitySpeed, int entityLuck, bool entityIsPlayer)
     {
         name = entityName;
+        isPlayer = entityIsPlayer;
         level = entityLevel;
 
         maxHealth = entityMaxHealth;
