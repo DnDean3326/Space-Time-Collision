98207b6713d33f06c25ceb179508633066469a25
DnDean3326 <94852868+DnDean3326@users.noreply.github.com>
2026-01-20 19:50:51 -0500

    Survey Link

--- Space-Time-Collision/Assets/_Scripts/Home Base/TutorialText.cs
+++ Space-Time-Collision/Assets/_Scripts/Home Base/TutorialText.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TutorialText : MonoBehaviour
 {
     [SerializeField] private GameObject tutorialBox;
+    [SerializeField] private GameObject victoryBox;
     private PlayerPrefs playerPrefs;
 
     private void Awake()
@@ -20,6 +21,13 @@ public class TutorialText : MonoBehaviour
         } else {
             tutorialBox.SetActive(false);
         }
+
+        if (playerPrefs.GetRunStatus() > 2) {
+            victoryBox.SetActive(true);
+            Time.timeScale = 0;
+        } else {
+            victoryBox.SetActive(false);
+        }
     }
     
     public void CloseTutorialBox()
@@ -27,5 +35,16 @@ public class TutorialText : MonoBehaviour
         playerPrefs.SetTutorialStatus(true);
         Destroy(tutorialBox);
         Time.timeScale = 1;
+    }
+
+    public void CloseVictoryBox()
+    {
+        Destroy(victoryBox);
+        Time.timeScale = 1;
+    }
+
+    public void SurveyLink()
+    {
+        Application.OpenURL("https://docs.google.com/forms/d/e/1FAIpQLSf-2VNk2eYvmdz76pQJCJaAQutHb3UHtDsMuBtV8ta4uV1-LA/viewform?usp=header");
     }
 }
